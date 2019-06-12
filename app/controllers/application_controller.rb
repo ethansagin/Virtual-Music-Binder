eeb99d89039a901229a31ad90cbccfc62d4a2388
Ethan Sagin <ethansagin@gmail.com>
2019-06-12 18:34:34 +0000

    create helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,18 @@ class ApplicationController < Sinatra::Base
   get "/" do
     erb :welcome
   end
+  
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+    
+    def curretn_user
+      if logged_in?
+        @current_user = User.find_by(id: session[:id])
+      end
+      @current_user
+    end
+  end
 
 end
