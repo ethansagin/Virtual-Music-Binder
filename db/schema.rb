786611f5c864bd51b3aefd47a82d102ead4f29e0
Ethan Sagin <ethansagin@gmail.com>
2019-06-12 18:22:53 +0000

    create folders table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190612181834) do
+ActiveRecord::Schema.define(version: 20190612182208) do
+
+  create_table "folders", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
