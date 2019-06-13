9776a13942aebee7ca855965d50e4a79a496a39e
Ethan Sagin <ethansagin@gmail.com>
2019-06-13 14:15:45 +0000

    create login/out routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,23 @@
 class SessionsController<ApplicationController
   
+  get '/sessions/login' do 
+    if logged_in?
+      redirect to '/folders'
+    else
+      erb :'sessions/login'
+    end
+  end
+  
+  # post '/sessions/login' do
+    
+  # end
+  
+  # get '/sessions/logout' do
+    
+  # end
+  
+  # post '/sessions/logout' do
+    
+  # end
+  
 end
