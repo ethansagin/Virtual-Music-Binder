6088cdc1535c3ea179c7212d1ea49d5a8733c5ac
Ethan Sagin <ethansagin@gmail.com>
2019-06-13 14:54:09 +0000

    link session to folder index erb

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,9 +1,9 @@
 class FoldersController<ApplicationController
   
   get '/folders' do
-  
-  erb :'folders/index'
-  
+    @user = User.find_by(id: session[:user_id])
+    
+    erb :'folders/index'
   end
   
 end
