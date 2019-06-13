2898a96bee17cf65ae81b4e7f209390440db6c81
Ethan Sagin <ethansagin@gmail.com>
2019-06-13 13:58:09 +0000

    write post signup path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,13 @@ class UsersController<ApplicationController
   end
   
   post '/users/signup' do
-    
+    params.each do |k, v|
+      if v.empty?
+        redirect to '/users/signup'
+      end
+    user = User.create(username: params[:username], email: params[:email] password: params[:password])
+    session[:user_id] = user.id
+    #redirect to '/folders/index'    
   end
   
 end
