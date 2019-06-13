43bf9ef696d3434ba291de6c0c535d04d25c519a
Ethan Sagin <ethansagin@gmail.com>
2019-06-13 14:12:28 +0000

    enable sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController<ApplicationController
   
   get '/users/signup' do
-    # if logged_in?
-    #   redirect to '/folders'
-    #else
-    erb :'users/signup'
+    if logged_in?
+      redirect to '/folders'
+    else
+      erb :'users/signup'
+    end
   end
   
   post '/users/signup' do
