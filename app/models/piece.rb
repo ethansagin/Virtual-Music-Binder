895f7ccfc02266e7fdf69c82a42f91e98c751899
Ethan Sagin <ethansagin@gmail.com>
2019-06-14 21:13:08 +0000

    add relation between user and piece models

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,4 +1,5 @@
 class Piece<ActiveRecord::Base
   has_many :folders_pieces
+  belongs_to :user
   has_many :folders, through: :folders_pieces
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User<ActiveRecord::Base
   has_many :folders
+  has_many :pieces
   has_secure_password
 end
